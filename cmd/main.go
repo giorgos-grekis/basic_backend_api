5fd9fe9a9449ad861cb8404ccb87c467c862b2e2
giorgos-grekis <georgrekis.github@gmail.com>
2024-12-30 13:24:11 +0200

    connect to database and add envs

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/giorgos-grekis/basic_backend_api/cmd/api"
+	"github.com/giorgos-grekis/basic_backend_api/config"
+	"github.com/giorgos-grekis/basic_backend_api/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB: Successfully connected!")
 }
